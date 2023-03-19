66e145ff2e1397c37e2cb493faf129b8101653ae
Daniel Korsah <danielkorsah97@gmail.com>
2023-03-19 04:21:07 +0000

    Add angle indicator

--- Assets/Scripts/DotProductVisualiser.cs
+++ Assets/Scripts/DotProductVisualiser.cs
@@ -12,6 +12,7 @@ public class DotProductVisualiser : ImmediateModeShapeDrawer
     public Line lineB;
     public Line projectionLine;
     public Line extensionLine;
+    public Disc angleArc;
 
     public TMP_Text dotOutputText;
     public TMP_Text angleOutputText;
@@ -19,6 +20,7 @@ public class DotProductVisualiser : ImmediateModeShapeDrawer
 
     private Vector3 a;
     private Vector3 b;
+    private float angle;
     
     public override void DrawShapes( Camera cam ){
 
@@ -28,14 +30,38 @@ public class DotProductVisualiser : ImmediateModeShapeDrawer
             GetVectors();
             SetProjectionLine();
             ShowNumbers();
+            ShowArc();
         }
+    }
+
+    private void ShowArc()
+    {
+        float signedAngleA = SignedAngleBetween(Vector3.right, a);
+        float signedAngleB = SignedAngleBetween(Vector3.right, b);
+
+        float angleDifference = Mathf.Abs(signedAngleB - signedAngleA);
+        float smallestAngle = Mathf.Min(angleDifference, 2 * Mathf.PI - angleDifference);
+
+        angleArc.AngRadiansStart = Mathf.Min(signedAngleA, signedAngleB);
+        angleArc.AngRadiansEnd = angleArc.AngRadiansStart + smallestAngle;
+
+        float smallestMagnitude = Mathf.Min(a.magnitude, b.magnitude);
+        angleArc.Radius = Mathf.Clamp(smallestMagnitude/3,  0.5f, 2f);
+    }
+
+    
+    private float SignedAngleBetween(Vector3 vectorA, Vector3 vectorB)
+    {
+        float unsignedAngle = Mathf.Acos(Vector3.Dot(vectorA.normalized, vectorB.normalized));
+        float sign = Mathf.Sign(Vector3.Dot(Vector3.Cross(vectorA, vectorB), Vector3.forward));
+        return unsignedAngle * sign;
     }
 
     private void ShowNumbers()
     {
         float dot = Vector3.Dot(a, b);
         dotOutputText.text = $"Dot Product: {dot}";
-        float angle = (Mathf.Acos(dot / (a.magnitude * b.magnitude)));
+        angle = (Mathf.Acos(dot / (a.magnitude * b.magnitude)));
         angleOutputText.text = $"Angle: {angle:0.##} rad  or {Mathf.Rad2Deg * angle:0.##}\u00B0";
         magnitudeOutputText.text = $"Projected Vector Magnitude: {Vector3.Dot(a, b) / b.magnitude:0.##}";
     }

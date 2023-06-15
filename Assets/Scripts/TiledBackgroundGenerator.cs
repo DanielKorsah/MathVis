80d3ad24d2e0c3e52f2052dd102bea0b68875c43
Daniel Korsah <danielkorsah97@gmail.com>
2023-06-15 16:08:17 +0100

    Make Background work in WebGl environment.

--- Assets/Scripts/TiledBackgroundGenerator.cs
+++ Assets/Scripts/TiledBackgroundGenerator.cs
@@ -1,8 +1,11 @@
+using System;
 using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
 using Shapes;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [ExecuteAlways]
 public class TiledBackgroundGenerator : ImmediateModeShapeDrawer
@@ -11,13 +14,106 @@ public class TiledBackgroundGenerator : ImmediateModeShapeDrawer
     public float lineWeight;
     public int size;
     
+#if  !UNITY_WEBGL
     public override void DrawShapes( Camera cam ){
+
 
         using( Draw.Command( cam ) ){
             MakeBackground();
             MakeLines();
         }
 
+    }
+#endif
+    public void Start()
+    {
+#if  UNITY_WEBGL
+        WebglBackground();
+        WebglLines();
+#else
+        DisableWebCompatibleObjects();
+#endif
+    }
+
+    private void DisableWebCompatibleObjects()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private void WebglBackground()
+    {
+        GameObject background;
+        Rectangle rect;
+        
+        if (transform.GetComponentInChildren<Rectangle>() == null)
+        {
+            background = new GameObject("Background Plane");
+            background.transform.parent = gameObject.transform;
+            rect = background.AddComponent<Rectangle>();
+        }
+        else
+        {
+            background = GetComponentInChildren<Rectangle>().GameObject();
+            rect = background.GetComponent<Rectangle>();
+        }
+
+        background.transform.position = new Vector3(0, 0, gameObject.transform.position.z);
+        rect.BlendMode = ShapesBlendMode.Opaque;
+        rect.Color = Color.black;
+        rect.Width = size;
+        rect.Height = size;
+    }
+
+    private void WebglLines()
+    {
+        Line[] preExistingLines = transform.GetComponentsInChildren<Line>();
+        if (preExistingLines.Length != 0)
+        {
+            return;
+        }
+        
+        //Vertical
+        for (int i = 0; i < (size/2)+1; i++)
+        {
+            Vector3 start = new Vector3(i, -size/2, 5);
+            Vector3 end = new Vector3(i, size/2, 5);
+            SpawnWebLine(start, end);
+        }
+
+        for (int i = -1; i > -(size/2)-1; i--)
+        {
+            Vector3 start = new Vector3(i, -size/2, 5);
+            Vector3 end = new Vector3(i, size/2, 5);
+            SpawnWebLine(start, end);
+        }
+        
+        //Horizontal
+        for (int i = 0; i < (size/2)+1; i++)
+        {
+            Vector3 start = new Vector3(-size/2, i, 5);
+            Vector3 end = new Vector3(size/2, i, 5);
+            SpawnWebLine(start, end);
+        }
+
+        for (int i = -1; i > -(size/2)-1; i--)
+        {
+            Vector3 start = new Vector3(-size/2, i, 5);
+            Vector3 end = new Vector3(size/2, i, 5);
+            SpawnWebLine(start, end);
+        }
+        
+    }
+
+    private void SpawnWebLine(Vector3 start, Vector3 end)
+    {
+        GameObject LineContainer = new GameObject("LineContainer");
+        LineContainer.transform.parent = gameObject.transform;
+        Line line = LineContainer.AddComponent<Line>();
+        line.BlendMode = ShapesBlendMode.Opaque;
+        line.Color = Color.grey;
+        line.Start = start;
+        line.End = end;
     }
 
     private void MakeBackground()
@@ -32,7 +128,7 @@ public class TiledBackgroundGenerator : ImmediateModeShapeDrawer
         Draw.LineGeometry = LineGeometry.Flat2D;
         Draw.ThicknessSpace = ThicknessSpace.Meters;
         Draw.Thickness = lineWeight;
-
+        
         // set static parameter to draw in the local space of this object
         Draw.Matrix = transform.localToWorldMatrix;
 
